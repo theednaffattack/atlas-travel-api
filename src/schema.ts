3ad02fda9ff172185af9b557b3f5bc2f28f94390
eddie naff <eddienaff@eddienaff.com>
2020-08-05 13:54:44 -0700

    Revert to one big resolver file

--- src/schema.ts
+++ src/schema.ts
@@ -1,24 +1,14 @@
 import "graphql-import-node";
 import { makeExecutableSchema } from "graphql-tools";
 import { GraphQLSchema } from "graphql";
-import { merge } from "lodash";
+// import { merge } from "lodash";
 
 import * as typeDefs from "./schema/schema.graphql";
-import { bookResolver } from "./graphql/resolvers/book-example";
-
-// import { resolvers } from "./resolver-map";
-
-const resolvers = {
-  Query: {
-    book: () => {
-      return { name: "A Sample Book" };
-    },
-  },
-};
+import { resolvers } from "./resolver-map";
 
 const schema: GraphQLSchema = makeExecutableSchema({
   typeDefs,
-  resolvers: merge(resolvers, bookResolver),
+  resolvers: resolvers, // merge(resolvers),
 });
 
 export default schema;
