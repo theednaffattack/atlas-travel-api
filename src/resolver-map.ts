83dedd0e22823dd4931569af174bb8412675e68f
eddie naff <eddienaff@eddienaff.com>
2020-08-05 13:55:05 -0700

    Improve reolver types

--- src/resolver-map.ts
+++ src/resolver-map.ts
@@ -1,34 +1,206 @@
 import { IResolvers } from "graphql-tools";
+import bcrypt from "bcrypt";
+
+import { highlight } from "sql-highlight/dist";
 
 import * as db from "./zapatos/src";
 import * as s from "./zapatos/schema";
 import pool from "./pg-pool";
 import { MyContext } from "./typings";
+import { transaction } from "./zapatos/src";
+import { AuthenticationError } from "apollo-server-express";
+
+interface LoginArgs {
+  email: string;
+  password: string;
+}
+
+const highlighterOptions = {
+  html: false,
+  classPrefix: "sql-hl-",
+  colors: {
+    keyword: "\x1b[35m",
+    function: "\x1b[31m",
+    number: "\x1b[32m",
+    string: "\x1b[32m",
+    special: "\x1b[33m",
+    bracket: "\x1b[33m",
+  },
+};
+
+function createNameField({
+  firstName,
+  lastName,
+}: {
+  firstName: string;
+  lastName: string;
+}): string {
+  return firstName + " " + lastName;
+}
+
+type UserType =
+  | {
+      name: string;
+      firstName: string;
+      id: string;
+      lastName: string;
+      profileImageUri: string | null;
+    }
+  | null
+  | undefined;
+
+interface MessageInterface {
+  id: string;
+  message?: string;
+  sentBy?: string | null;
+  userId?: string | null;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+type MessagesSubType = MessageInterface[] | null;
 
 export const resolvers: IResolvers = {
   Query: {
     helloWorld(_: void, args: void): string {
       return `👋🏾 Hello world! 👋🏾`;
     },
-    async me(parent, args, context: MyContext, info) {
-      // if we can't find a userId on the current session
+    async me(_parent, _args, context: MyContext): Promise<UserType> {
+      // if we can't find a userId on the current session => undefined
       if (!context.req.session!.userId) {
         return undefined;
       }
 
-      // selectExactlyOne, Whereable
-      // for a more useful example, see the section on `lateral`, below
       try {
-        const exactlyOneUser = await db
-          .selectExactlyOne("user", { id: context.userId })
+        const getMyDetails = await db
+          .selectOne("user", { id: context.userId })
           .run(pool);
         // ... do something with this user ...
-        return exactlyOneUser;
+
+        if (!getMyDetails) {
+          throw new AuthenticationError("Not authenticated!");
+        }
+
+        const user = {
+          name: createNameField({
+            firstName: getMyDetails.firstName,
+            lastName: getMyDetails.lastName,
+          }),
+          ...getMyDetails,
+        };
+
+        return user;
       } catch (err) {
         if (err instanceof db.NotExactlyOneError)
           console.log(`${err.name}: ${err.message}`);
         else throw err;
       }
+    },
+    async getMyMessages(
+      _parent,
+      _args,
+      context: MyContext
+    ): Promise<MessagesSubType> {
+      try {
+        const messages = await db.sql<s.message.SQL, s.message.Selectable[]>`
+        SELECT * FROM ${"message"} WHERE ${"userId"} = ${db.param(
+          context.userId
+        )} OR ${"sentBy"} = ${db.param(context.userId)}
+        `.run(pool);
+
+        console.log("CAN I SEE MESSAGES\n", messages);
+
+        return messages;
+        // const theMessages = await db
+        //   .select("message", { userId: context.userId })
+        //   .run(pool);
+      } catch (error) {
+        // if (error instanceof db.NotExactlyOneError)
+        console.log(`${error.name}: ${error.message}`);
+        throw error;
+      }
+    },
+  },
+  Mutation: {
+    async login(
+      _,
+      { email, password }: LoginArgs,
+      context: MyContext
+    ): Promise<UserType> {
+      const { req } = context;
+
+      let user;
+
+      try {
+        // START - logging
+
+        await db.setConfig({
+          queryListener: (argThing) => {
+            const highlightThing = highlight(argThing.text, highlighterOptions);
+            console.log("\n=====================");
+            console.log("START QUERY LISTENER");
+            console.log("=====================\n");
+            console.log("QUERY");
+            console.log(highlightThing);
+            console.log("\nVALUES");
+            console.log(argThing.values);
+            console.log("\n=====================");
+            console.log("END QUERY LISTENER");
+            console.log("=====================\n");
+          },
+          resultListener: (argThing) => {
+            console.log("\n=====================");
+            console.log("START RESULTS LISTENER");
+            console.log("=====================\n");
+            console.log(argThing);
+            console.log("\n=====================");
+            console.log("END RESULTS LISTENER");
+            console.log("=====================\n");
+          },
+        });
+
+        // END - logging
+
+        const tempUser = await db.selectOne("user", { email }).run(pool);
+        if (!tempUser) {
+          throw new Error("Could not find user!");
+        }
+        user = {
+          name: createNameField({
+            firstName: tempUser.firstName,
+            lastName: tempUser.lastName,
+          }),
+          ...tempUser,
+        };
+      } catch (error) {
+        console.log("ERROR", error);
+        if (error instanceof db.NotExactlyOneError) {
+          console.log(`${error.name}: ${error.message}`);
+        }
+        throw new Error(error);
+      }
+
+      // if the user can't be found return early
+      if (!user) {
+        return null;
+      }
+
+      const valid = await bcrypt.compare(password, user.password);
+
+      // if the supplied password is invalid return early
+      if (!valid) {
+        return null;
+      }
+
+      // if the user has not confirmed via email
+      if (!user.confirmed) {
+        throw new Error("Please confirm your account");
+        // return null;
+      }
+
+      // all is well return the user we found
+      req.session!.userId = user.id;
+      return user;
     },
   },
 };
