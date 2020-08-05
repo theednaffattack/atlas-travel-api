a8c05fb7c7573d57bdc7dde633afdd2730de7e69
eddie naff <eddienaff@eddienaff.com>
2020-08-05 13:53:47 -0700

    Add redis

---
+++ src/redis.ts
@@ -0,0 +1,11 @@
+import Redis from "ioredis";
+
+export const redis = new Redis({
+  host:
+    process.env.NODE_ENV === "production"
+      ? process.env.REDIS_HOST
+      : "localhost",
+  port: process.env.REDIS_PORT_NUMBER
+    ? parseInt(process.env.REDIS_PORT_NUMBER)
+    : 6379,
+});
