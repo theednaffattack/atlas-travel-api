ca762c5f085242738ee5816a421380180b374f54
eddie naff <eddienaff@eddienaff.com>
2020-08-06 16:40:50 -0700

    Add test utils

---
+++ src/test-utils/gql-call.ts
@@ -0,0 +1,45 @@
+import { graphql } from "graphql";
+import { Maybe } from "graphql/jsutils/Maybe";
+
+import schema from "../schema";
+
+// prettier-ignore
+interface Options {
+  source: string;
+  variableValues?: Maybe<{
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [key: string]: any;
+  }>;
+  userId?: string;
+}
+
+// let schema: GraphQLSchema;
+
+export const gqlCall = async ({
+  source,
+  variableValues,
+  userId,
+}: // eslint-disable-next-line @typescript-eslint/no-explicit-any
+Options): Promise<any> => {
+  // if (!schema) {
+  //   schema = await createSchema();
+  // }
+
+  return graphql({
+    schema,
+    source,
+
+    variableValues,
+    contextValue: {
+      req: {
+        session: {
+          userId,
+        },
+      },
+      res: {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        clearCookie: (): jest.Mock<any, any> => jest.fn(),
+      },
+    },
+  });
+};
