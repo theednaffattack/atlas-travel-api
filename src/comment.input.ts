920871ad07681522b369b64ad872d1b8452a68e0
Eddie Naff <eddie@eddienaff.dev>
2020-09-05 21:24:45 -0700

    Add type-graphql example code

---
+++ src/comment.input.ts
@@ -0,0 +1,15 @@
+import { Field, ID, InputType } from "type-graphql";
+
+import { Comment } from "./comment.type";
+
+@InputType()
+export class CommentInput implements Partial<Comment> {
+  @Field(() => ID)
+  recipeId!: string;
+
+  @Field({ nullable: true })
+  nickname?: string;
+
+  @Field()
+  content!: string;
+}
