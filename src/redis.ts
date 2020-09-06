38023670eb67eae1ec5af1881e8e0dbe5fa5549d
Eddie Naff <eddie@eddienaff.dev>
2020-09-05 21:38:20 -0700

    Export standard redis connection options

--- src/redis.ts
+++ src/redis.ts
@@ -1,5 +1,10 @@
 import Redis from "ioredis";
 
+export const redisHostAndPortOpts = {
+  host: process.env.NODE_ENV === "production" ? process.env.REDIS_HOST : "localhost",
+  port: process.env.REDIS_PORT_NUMBER ? parseInt(process.env.REDIS_PORT_NUMBER) : 6379,
+};
+
 export const redis = new Redis({
   host: process.env.NODE_ENV === "production" ? process.env.REDIS_HOST : "localhost",
   port: process.env.REDIS_PORT_NUMBER ? parseInt(process.env.REDIS_PORT_NUMBER) : 6379,
