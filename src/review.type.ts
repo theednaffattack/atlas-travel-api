1100c931bf487c4bcdab9d0c6cfacb701a56a4d6
Eddie Naff <eddie@eddienaff.dev>
2020-09-06 16:34:45 -0700

    Add Review entity

---
+++ src/review.type.ts
@@ -0,0 +1,47 @@
+import { ObjectType, Field, Float, ID } from "type-graphql";
+
+import { User } from "./user.type";
+import { Hotel } from "./hotel.type";
+import { Venue } from "./venue";
+
+// prettier-ignore
+@ObjectType()
+export class Review  {
+  @Field(() => ID)
+  id!: string;
+  
+  @Field()
+  createdAt!: Date;
+
+  @Field()
+  updatedAt!: Date;
+
+
+  
+  @Field(() => Float)
+  value!: number;
+
+  @Field()
+  title!: string;
+
+  @Field()
+  text!: string;
+  
+  @Field({ nullable!: true })
+  userId!: string;
+
+  @Field(() => User)
+  user!: User;
+
+  @Field({ nullable!: true })
+  hotelId!: string;
+  
+  @Field(() => Hotel)
+  hotel!: Hotel;
+
+  @Field({ nullable!: true })
+  venueId!: string;
+  
+  @Field(() => Venue)
+  venue!: Venue;
+}
