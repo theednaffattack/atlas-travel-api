8dad833bfd44457900c7c8683425121b6dab3fce
Eddie Naff <eddie@eddienaff.dev>
2020-09-09 16:43:55 -0700

    Add input type to edit user info

---
+++ src/user.edit-user-info.input.ts
@@ -0,0 +1,20 @@
+import { Length, IsEmail } from "class-validator";
+import { Field, InputType, ID } from "type-graphql";
+
+@InputType()
+export class EditUserInput {
+  @Field(() => String)
+  @Length(1, 255)
+  firstName!: string;
+
+  @Field(() => String)
+  @Length(1, 255)
+  lastName!: string;
+
+  @Field(() => String)
+  @IsEmail()
+  email?: string;
+
+  @Field(() => ID)
+  teamId!: string;
+}
