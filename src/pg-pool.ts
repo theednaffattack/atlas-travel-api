ff5a92a26bf7edfbcb6034d40561ad95015f5381
Eddie Naff <eddie@eddienaff.dev>
2020-09-09 12:08:48 -0700

    Code cleanup, remove comments

--- src/pg-pool.ts
+++ src/pg-pool.ts
@@ -1,16 +1,5 @@
 import pg from "pg";
 
 export default new pg.Pool({
-  connectionString: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_USER}@localhost:5432/${process.env.POSTGRES_DBNAME}`,
-
-  // connectionString: process.env.PG_CONNECTION_STRING,
-  // connectionString:
-  //   "postgres://at_system:at_system@localhost:5432/atlas_travel",
-  // // connectionString: "postgresql://localhost/atlas_travel",
-
-  // user: process.env.POSTGRES_USER,
-  // host: "localhost",
-  // database: process.env.POSTGRES_DBNAME,
-  // password: process.env.POSTGRES_PASS,
-  // port: process.env.POSTGRES_PORT,
-}); // process.env.DATABASE_URL });
+  connectionString: `${process.env.PG_PROD_CONNECTION_STRING}`,
+});
