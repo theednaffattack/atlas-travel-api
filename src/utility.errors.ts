fc31e9940ace87abfa958f052cc961e5cd970d5c
Eddie Naff <eddie@eddienaff.dev>
2020-09-10 12:58:25 -0700

    Add optional 'error' property to saving info error

--- src/utility.errors.ts
+++ src/utility.errors.ts
@@ -1,5 +1,5 @@
 export const nodeEnvIsUndefined = (functionName: string): string =>
   `The NODE_ENV var is undefined. The "${functionName}" expects to receive NODE_ENV as an argument.  Please set your environment variable "NODE_ENV" to "development", "production" or "test" and try again.`;
 
-export const errorSavingInfoToDatabase = (functionName?: string): string =>
-  `${functionName} experienced an error while saving your information to the database.`;
+export const errorSavingInfoToDatabase = (functionName?: string, error?: unknown | string): string =>
+  `${functionName} experienced an error while saving your information to the database.\n${error}`;
