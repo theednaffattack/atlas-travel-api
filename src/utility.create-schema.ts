97a54140d2abcba31c6678fa19993b41716bbd70
Eddie Naff <eddie@eddienaff.dev>
2020-11-14 09:07:33 -0800

    Rearrange imports

--- src/utility.create-schema.ts
+++ src/utility.create-schema.ts
@@ -1,6 +1,8 @@
 import { buildSchema } from "type-graphql";
 import { GraphQLSchema } from "graphql";
 import "reflect-metadata";
+import Redis, { RedisOptions } from "ioredis";
+import { RedisPubSub } from "graphql-redis-subscriptions";
 
 import { MeResolver } from "./me.resolver";
 import { LoginResolver } from "./user.login.resolver";
@@ -8,9 +10,7 @@ import { RegisterResolver } from "./user.register.resolver";
 import { ChangePasswordFromTokenResolver } from "./user.change-password-from-token";
 import { ChangePasswordFromContextUseridResolver } from "./user.change-password-from-context";
 import { RecipeResolver } from "./recipe.resolver";
-import Redis, { RedisOptions } from "ioredis";
 import { redisHostAndPortOpts } from "./redis";
-import { RedisPubSub } from "graphql-redis-subscriptions";
 import { AdminEditAnotherUser_sInfoResolver } from "./user.admin.edit-another-user_s-user-info";
 import { customAuthorizationChecker } from "./utility.custom-authorization-checker";
 
